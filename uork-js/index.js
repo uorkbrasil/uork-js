363fc3999efd6290e8a67263101fc56a00430183
Aligg <114702742+henriqueamorimm@users.noreply.github.com>
2023-06-27 21:32:07 -0300

    Add files via upload

---
+++ uork-js/index.js
@@ -0,0 +1,44 @@
+/* 
+The following codes are to allow you to suggest modifications on the Uork Web App.
+
+Follow the GET redirects:
+
+
+
+Obs: Remember to PR the repository :)
+*/
+
+
+
+
+const configs = {
+    redirects: {
+      login: ["login","LoginForm"],
+      register: "registro",
+      show_worker_profile: ["exibir","SearchExibir"],
+      home: "inicio",
+      notifications: "notifications",
+      profile: "perfil",
+    },
+    payments: {
+     pix: ["adicionarsaldo/Pix", "adicionarsaldo"],
+     gift_card: ["reedemcode"]
+    },
+    admin: {
+        admin: ["admin"]
+    },
+    articles: {
+        example: ["example"]
+    },
+    smartphones_home: {
+        ios_start: "ios-tutorial",
+        android_start: "https://play.google.com/store/apps/details?id=com.app.youworkapp2",
+    },
+    smartphone_notifications: { 
+        ios_notifications: "notifications",
+        android_notifications: "noticications"
+    }
+
+}
+console.log("Redirect´s uork:");
+console.log(configs)
